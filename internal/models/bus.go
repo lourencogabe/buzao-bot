f19f2546e575e102285a0639b91a35539e2d8ec7
Pablo Pasquim <pablo.pasquim7@gmail.com>
2026-04-14 15:02:09 -0300

    feat: :sparkles: Add new struct for project and create bus model

---
+++ internal/models/bus.go
@@ -0,0 +1,10 @@
+package models
+
+type BusLine struct {
+    ID          int
+    Number      string
+    Description string
+    UrlUrbs     string
+    UrlGazeta   string
+    UrlMoovit   string
+}
